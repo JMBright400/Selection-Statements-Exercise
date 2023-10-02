93a61e26de38049a45bbcc35608b0cafd3b1aea8
John Bright <jmbright400@gmail.com>
2023-10-02 13:52:33 -0500

    Set my favorite number to 300.

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favNumber = 300;
+
+          
+
+            Console.WriteLine("Guess my favorite number between 1 and 1000:");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            { Console.WriteLine("Too low."); }
+
+            else if (userInput > favNumber)
+            { Console.WriteLine("Too high."); }
+
+            else
+            { Console.WriteLine("You guessed it!"); }
         }
     }
 }
